8522d4214d538cdcf63291e0ad2a0ac200e5666f
jkassande <jdkasdel@gmail.com>
2025-03-05 16:51:57 +0000

    feat: add admin dashboard for displaying main informations

--- src/suiviPresence/urls.py
+++ src/suiviPresence/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.accueil, name='accueil'),
+    path('interface/admin/', views.interface_admin, name='interface_admin'),
 ]
